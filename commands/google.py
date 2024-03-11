833c69e081c3b11b64f12a4548192be859306d1b
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-03-10 23:07:55 -0500

    moves related commands into the same command files, jarvis now listens for name silently then responds to single command before going back to silently listening for name

--- commands/google.py
+++ commands/google.py
@@ -3,6 +3,13 @@ from .speak import speak
 import urllib.parse
 import webbrowser
 
+# Function for opening google
+def open_google(): 
+    statement = 'Opening...'
+    print(statement)
+    speak(statement)
+    webbrowser.open('https://www.google.com')
+
 # Function for seaerching google
 def search_google(): 
     response = 'Yes sir, what should I search?'
@@ -11,6 +18,6 @@ def search_google():
     query = take_command().lower()
     encoded_query = urllib.parse.quote(query)
     webbrowser.open(f'https://www.google.com/search?q={encoded_query}')
-    response = 'Yes sir, here are your search results'
+    response = 'Here are your search results'
     print(response)
     speak(response)
--- commands/open_google.py
+++
@@ -1,9 +0,0 @@
-from .speak import speak
-import webbrowser
-
-# Function for opening google
-def open_google(): 
-    statement = 'Opening...'
-    print(statement)
-    speak(statement)
-    webbrowser.open('https://www.google.com')
